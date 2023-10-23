3ebb283923e41af5927c3801c43f37a31febdb3d
artur martinho <arturmartinhobr2004@gmail.com>
2023-10-23 14:53:35 -0300

    tela preta

---
+++ jogo/tela_inicial.py
@@ -0,0 +1,25 @@
+import pygame
+def inicializa():
+    pygame.init()
+    window = pygame.display.set_mode((800,800))
+    pygame.display.set_caption('JOGO DA COBRINHA')
+    return window
+
+def recebe_eventos():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+    return True
+
+def desenha(window):
+    window.fill((0,0,0))
+    
+    
+    pygame.display.update()
+
+def game_loop(window):
+    while recebe_eventos():
+        desenha(window)
+    
+w = inicializa()
+game_loop(w)
