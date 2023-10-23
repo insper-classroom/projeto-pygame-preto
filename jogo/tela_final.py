e9d2baf32426d93947731b69b90212a2a846e806
Mona Lisa <aninhabiac123@gmail.com>
2023-10-23 19:27:35 -0300

    adicionei novas imagens, arrumei a tela inicial e final

--- jogo/tela_final.py
+++ jogo/tela_final.py
@@ -1,18 +1,12 @@
 import pygame
+
 def inicializa():
-    #janela
     pygame.init()
     window = pygame.display.set_mode((800,700))
     pygame.display.set_caption('JOGO DA COBRINHA')
 
     dicionario = {}
-
-    #desenho da cobra
-    dicionario['cobra'] = pygame.image.load('imagens/cobra.png')
-    dicionario['cobra'] = pygame.transform.scale(dicionario['cobra'],(200,200))
-
-    #imagem bem vindos
-    dicionario['bem vindo'] = pygame.image.load('imagens/bem vindos.png')
+    dicionario['game over'] = pygame.image.load('imagens/game over.png')
 
     return window,dicionario
 
@@ -25,14 +19,13 @@ def recebe_eventos():
 def desenha(window,dicionario):
     window.fill((0,149,0))
     
-    window.blit(dicionario['cobra'],(170,180))
-    window.blit(dicionario['bem vindo'],(0,0))
-    
+    window.blit(dicionario['game over'],(120,190))
+
     pygame.display.update()
 
 def game_loop(window,dicionario):
     while recebe_eventos():
         desenha(window,dicionario)
     
-w,d= inicializa()
+w,d = inicializa()
 game_loop(w,d)
