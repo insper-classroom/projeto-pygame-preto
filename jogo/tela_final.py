d4943324043adf2850065a2429d870c90e9d2a2e
Mona Lisa <aninhabiac123@gmail.com>
2023-10-24 16:30:05 -0300

    adicionei novas imagens,finalizei a tela final e comecei do chefao

--- jogo/tela_final.py
+++ jogo/tela_final.py
@@ -8,6 +8,11 @@ def inicializa():
     dicionario = {}
     dicionario['game over'] = pygame.image.load('imagens/game over.png')
 
+    dicionario['caveira'] = pygame.image.load('imagens/caveira.png')
+    dicionario['caveira'] = pygame.transform.scale(dicionario['caveira'],(130,130))
+
+    dicionario['fonte'] = pygame.font.Font('fonte/retro_computer_personal_use.ttf',15)
+
     return window,dicionario
 
 def recebe_eventos():
@@ -19,7 +24,12 @@ def recebe_eventos():
 def desenha(window,dicionario):
     window.fill((0,149,0))
     
-    window.blit(dicionario['game over'],(120,190))
+    window.blit(dicionario['game over'],(120,50))
+
+    window.blit(dicionario['caveira'],(300,220))
+
+    texto = dicionario['fonte'].render('Pressione ENTER para recomecar',False,(0,0,0))
+    window.blit(texto,(200,480))
 
     pygame.display.update()
 
