566b14d8ed4465de19c843be290e2994a564072b
Mona Lisa <aninhabiac123@gmail.com>
2023-11-06 15:25:18 -0300

    arrumei a tela inicial

--- jogo/tela_inicial.py
+++ jogo/tela_inicial.py
@@ -8,11 +8,11 @@ def inicializa():
     #janela
     pygame.init()
     window = pygame.display.set_mode((1200,800))
-    pygame.display.set_caption('JOGO DA COBRINHA')
+    pygame.display.set_caption('RETRO SNAKE')
 
     dicionario = {}
     #INSTRUCOES FONTE
-    dicionario['fonte'] = pygame.font.Font('fonte/retro_computer_personal_use.ttf',15)
+    dicionario['fonte'] = pygame.font.Font('fonte/perfect_dos_vga_437/Perfect DOS VGA 437 Win.ttf',25)
 
     #desenho da cobra
     dicionario['cobra'] = pygame.image.load('imagens/cobra_logo.png')
@@ -21,17 +21,12 @@ def inicializa():
     #imagem bem vindos
     dicionario['bem vindo'] = pygame.image.load('imagens/bem vindos.png')
 
-    #IMAGEM START
-    dicionario['start'] = pygame.image.load('imagens/botao_start.png')
-
     dicionario['int'] = 'sons/8bit-music-for-game-68698.mp3'
     pygame.mixer.music.load(dicionario['int'])
-    pygame.mixer.music.play()
+    pygame.mixer.music.play(-1)
     dicionario_comida = {}
 
     return window,dicionario,dicionario_comida
-    
-   
 
 def recebe_eventos():
     global vaijogo
@@ -48,21 +43,37 @@ def recebe_eventos():
                 vaijogo = True
                 
 
-
     return True
-
 
 def desenha(window,dicionario):
     window.fill((0,149,0))
 #DESENHAR A COBRA
     window.blit(dicionario['cobra'],(420,220))
-    window.blit(dicionario['bem vindo'],(360,0))
+
+#desenha bem vindo
+    window.blit(dicionario['bem vindo'],(360,100))
+
 #DESENHAR AS INSTRUÇOES
-    texto = dicionario['fonte'].render('Clique ENTER para iniciar',False,(0,0,0))
-    window.blit(texto,(430, 590))
-#DESENHAR O START
-    window.blit(dicionario['start'],(490,590))
-    
+    texto = dicionario['fonte'].render('COMO JOGAR:',False,(0,0,0))
+    window.blit(texto,(100,490))
+
+    texto = dicionario['fonte'].render('- você pode mover com as teclas W,S,A,D ou usar as setas ',False,(0,0,0))
+    window.blit(texto,(100,525))
+
+    texto = dicionario['fonte'].render('- para você aumentar o corpo é necessário comer a maça vermelha ',False,(0,0,0))
+    window.blit(texto,(100,550))
+
+    texto = dicionario['fonte'].render('- o objetivo do jogo é que você chegue na tela do chefão',False,(0,0,0))
+    window.blit(texto,(100,575))
+
+    texto = dicionario['fonte'].render('- para ir na tela do chefao precisa 20xp que ganha comendo a maça laranja',False,(0,0,0))
+    window.blit(texto,(100,600))
+
+    texto = dicionario['fonte'].render('- cuidado!! ao enfrentar o chefao você precisa ter 20xp para remover ele',False,(0,0,0))
+    window.blit(texto,(100,625))
+
+    texto = dicionario['fonte'].render('Clique ENTER para iniciar e bom jogo!',False,(0,0,0))
+    window.blit(texto,(100, 675))
     pygame.display.update()
 
 def game_loop_inicial(window,dicionario,estado):
