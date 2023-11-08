546941243743627cf0f272eb800f0fd1559da76e
artur martinho <arturmartinhobr2004@gmail.com>
2023-11-08 10:07:51 -0300

    xp 80

--- jogo/tela_jogo.py
+++ jogo/tela_jogo.py
@@ -174,7 +174,7 @@ def game_loop(window,dicionario,estado):
     # jogo começa
     while recebe_eventos(estado,dicionario,window):
         estado['clock'].tick(4)
-        if estado['xp'] <= 40:
+        if estado['xp'] <= 80:
             desenha(window,dicionario,estado)
         else:
             window,dicionario,estado = tela_chefão.inicializa()
